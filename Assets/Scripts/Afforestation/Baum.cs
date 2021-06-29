1024650013e94be4c37617f4aa8859e2957ad209
l-ine <line@pauwau.de>
2021-06-29 15:03:38 +0200

    29-juni_baumaufgabe_klasse_baum

--- Assets/Scripts/Afforestation/Baum.cs
+++ Assets/Scripts/Afforestation/Baum.cs
@@ -4,44 +4,17 @@ using UnityEngine;
 
 public class Baum : MonoBehaviour
 {
-    //public GameObject tree;
-    /*public float passedTime = 0f;
-    public Vector3 beginningScale = new Vector3(0.1f, 0.1f, 0.1f);*/
-
-    // Start is called before the first frame update
-    void Start()
+    public float passedTime = 0f;
+    
+    public void grow(Vector3 scale)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    /*public void grow()
-    {
+        //this.passedTime = 0f;
         this.passedTime += Time.deltaTime;
         if (this.passedTime < 10.0f)
         {
-
-            //for (int i = 0; i <= counter;  i++)
-            //{
-
             this.transform.localScale = Vector3.Lerp(new Vector3(0.2f, 0.2f, 0.2f),
-                                                        beginningScale, // oder:
-                                                                        // Tree[i].transform.localScale,
-                                                        1 / this.passedTime);
-            //Debug.Log("counter: " + this.counter);
-            //Debug.Log("scale " + this.counter + ": " + Tree[this.counter].transform.localScale);
-            //counter++;
-
-            //}
-
-
+                                                          scale,
+                                                          1 / this.passedTime);
         }
-
-
-    }*/
+    }
 }
--- Assets/Scripts/Afforestation/EventListener.cs
+++ Assets/Scripts/Afforestation/EventListener.cs
@@ -4,31 +4,28 @@ using UnityEngine;
 
 public class EventListener : MonoBehaviour
 {
-    public GameObject[] Tree;
-    //public Baum tree;
+    public Baum[] tree;
 
     private bool growing = false;
     
-    private float passedTime = 0f;
-
-    private Vector3 beginningScale = new Vector3(0.1f, 0.1f, 0.1f);
-
     private int counter = -1;
+
+    private Vector3[] scale;
 
     void Start()
     {
         EventSystemBase.aCollisionEvent += this.processCollisionEvent;
-        //tree = GameObject.Find("Oak_Tree(1)").GetComponent<Baum>();
     }
 
     void Update()
     {
-        if (this.growing)
+        if (this.growing && counter < tree.Length)
         {
-            this.grow();
+            for (int i = 0; i <= counter; i++)
+            {
+                tree[i].grow(scale[i]);
+            }
         }
-        Debug.Log("eventlistener");
-      
     }
 
     void processCollisionEvent(string type)
@@ -36,49 +33,9 @@ public class EventListener : MonoBehaviour
         if (type == "grow")
         {
             this.growing = true;
-            this.passedTime = 0.0f;
-            // counter hier hochzählen lassen, nicht in grow()
             counter++;
-
-            Debug.Log("counter: " + this.counter);
-        }
-
-    }
-
-    private void grow()
-    {
-        this.passedTime += Time.deltaTime;
-        if (this.counter < Tree.Length)
-        {
-            if (this.passedTime < 10.0f)
-            {
-
-                //for (int i = 0; i <= counter;  i++)
-                //{
-
-                Tree[counter].transform.localScale = Vector3.Lerp(new Vector3(0.2f, 0.2f, 0.2f),
-                                                            beginningScale, // oder:
-                                                                            // Tree[i].transform.localScale,
-                                                            1 / this.passedTime);
-                //Debug.Log("counter: " + this.counter);
-                Debug.Log("scale " + this.counter + ": " + Tree[this.counter].transform.localScale);
-                //counter++;
-
-                //}
-
-
-            }
+            this.scale = new Vector3[] { tree[0].transform.localScale, tree[1].transform.localScale, tree[2].transform.localScale };
         }
     }
-
-    /*private void grow()
-    {
-        this.passedTime += Time.deltaTime;
-        //Tree.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
-        while (Tree.transform.localScale.x < 0.8)
-        {
-            Tree.transform.localScale = beginningScale * Time.deltaTime;
-        }
-    }*/
 }
 
